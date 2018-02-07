92cc07128d513d9b1a234d0af73378139f676c4f
Cuiwenchang <756661204@qq.com>
2018-02-07 15:18:14 +0800

    u

---
+++ k8s/node/calico/rr.sh
@@ -0,0 +1,8 @@
+docker run -privileged -net=host -d                                \
+           -e IP=192.168.57.5                                      \
+           -e ETCD_ENDPOINTS=https://192.168.57.2:2379,https://192.168.57.3:2379,https://192.168.57.4:2379                \
+           -v /etc/etcd/certs:/etc/etcd/certs                  \
+           -e ETCD_CA_CERT_FILE=/etc/etcd/certs/base/ca.pem              \
+           -e ETCD_CERT_FILE=/etc/etcd/certs/client/client.pem               \
+           -e ETCD_KEY_FILE=/etc/etcd/certs/client/client-key.pem                 \
+           calico/routereflector
