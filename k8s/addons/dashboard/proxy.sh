95472aff78ad1d829304daae1a8519c3ed4b4e87
Cuiwenchang <756661204@qq.com>
2018-02-08 14:13:47 +0800

    u

---
+++ k8s/addons/dashboard/proxy.sh
@@ -0,0 +1,4 @@
+“kubectl proxy --address='172.20.0.113' --port=8086 --accept-hosts='^*$'
+
+
+“http://172.20.0.113:8086/api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard/#/workload?namespace=default”
